6e4999f4a681eb04ea66be1070a297516b1ecd56
Jakub WQ <jakub@makutynowicz.pl>
2026-03-17 15:27:38 +0100

    Pierwszy test

---
+++ Source/Billard/Billard.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Billard.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Billard, "Billard" );
